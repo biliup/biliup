eee737a81d21483f8ae5b9875875969b8c2a7a00
ForgQi <34411314+ForgQi@users.noreply.github.com>
2018-06-02 17:17:25 +0800

    re

--- eventdriven/__init__.py
+++ eventdriven/__init__.py
@@ -15,3 +15,35 @@ class Putevent(object):
                 _event = eventType.Event(type_)
                 self.eventManager.put(_event)
                 time.sleep(interval)
+
+
+def func(event):
+    print('running'+event.type_)
+    time.sleep(10)
+    print('done'+event.type_)
+def fun(event):
+    print('running'+event.type_)
+    time.sleep(10)
+    print('done'+event.type_)
+if __name__ == '__main__':
+    manager = event.EventEngine()
+
+    e = eventType.Event(type_='1')
+    e1 = eventType.Event(type_='2')
+    e2 = eventType.Event(type_='3')
+    e3 = eventType.Event(type_='4')
+
+    manager.register(e.type_, func)
+    manager.register(e.type_, fun)
+    manager.register(e1.type_, func)
+    manager.register(e2.type_, func)
+    manager.register(e3.type_, func)
+    manager.start()
+    print('go')
+    while True:
+        for i in event.d:
+            _event = eventType.Event(i)
+            manager.put(_event)
+            # print('put'+i)
+            print(event.d)
+            time.sleep(1)
