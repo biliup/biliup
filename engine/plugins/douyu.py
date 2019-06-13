300c0d335a5ebb98d5628faa4a3d3167a9ac6af8
cyh <709962401@qq.com>
2019-06-13 21:49:18 +0800

    修改斗鱼api

--- engine/plugins/douyu.py
+++ engine/plugins/douyu.py
@@ -6,9 +6,8 @@ import requests
 from engine.plugins import FFmpegdl, match1
 from engine.plugins.twitch import headers
 from common import logger
-
-VALID_URL_BASE = r'(?:https?://)?(?:(?:www|m)\.)?douyu\.com'
-_API_URL = "http://www.douyutv.com/api/v1/"
+from ykdl.common import url_to_module
+# 详细api见 https://github.com/zhangn1985/ykdl/blob/master/cykdl/__main__.py
 
 
 class Douyu(FFmpegdl):
@@ -16,47 +15,12 @@ class Douyu(FFmpegdl):
         super().__init__(fname, url, suffix)
 
     def check_stream(self):
-        # check_url = re.sub(r'.*douyu.com', 'http://open.douyucdn.cn/api/RoomApi/room', self.url)
-        # res = requests.get(check_url, timeout=5)
-        # res.close()
-        # s = res.json()
-        # logger.debug(self.fname)
-        # status = s['data']['room_status']
-        # if status == '2':
-        #     return False
-        # else:
-        #     return True
-        logger.debug(self.fname)
-        url = re.sub(r'.*douyu.com', 'https://m.douyu.com/room', self.url)
-        res = requests.get(url, timeout=5)
-        res.close()
-        html = res.text
-        room_id_patt = r'"rid"\s*:\s*(\d+),'
-        room_id = match1(html, room_id_patt)
-        if room_id == "0":
-            room_id = url[url.rfind('/') + 1:]
-        args = "room/%s?aid=wp&client_sys=wp&time=%d" % (room_id, int(time.time()))
-        auth_md5 = (args + "zNzMV1y4EMxOHS6I5WKm").encode("utf-8")
-        auth_str = hashlib.md5(auth_md5).hexdigest()
-        json_request_url = "%s%s&auth=%s" % (_API_URL, args, auth_str)
-        # print(json_request_url)
-        content = requests.get(json_request_url, headers=headers, timeout=5)
-        content.close()
-        # content = get_content(json_request_url, headers)
-        # print(content.text)
-        json_content = json.loads(content.text)
-        data = json_content['data']
-        server_status = json_content.get('error', 0)
-        if server_status is not 0:
-            raise ValueError("Server returned error:%s" % server_status)
-
-        title = data.get('room_name')
-        logger.debug(title)
-        show_status = data.get('show_status')
-        if show_status is not "1":
-            # raise ValueError("The live stream is not online! (Errno:%s)" % server_status)
-            return
-        self.ydl_opts['absurl'] = data.get('rtmp_url') + '/' + data.get('rtmp_live')
+        m, u = url_to_module(self.url)
+        parser = m.parser
+        info = parser(u)
+        stream_id = info.stream_types[0]
+        urls = info.streams[stream_id]['src']
+        self.ydl_opts['absurl'] = urls[0]
         return True
 
 
