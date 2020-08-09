9d6271c18fa941bec360ef736a051cd86ce972f2
waitting <1592021024@qq.com>
2020-08-09 20:51:57 +0800

    Update afreeca.py

--- engine/plugins/afreeca.py
+++ engine/plugins/afreeca.py
@@ -1,16 +1,57 @@
-# import json
-# import re
 import requests
-from engine.plugins import YDownload, BatchCheckBase, SDownload
+from engine.plugins import FFmpegdl
 from common import logger
+import re
 
-headers = {
-    'client-id': '5qnc2cacngon0bg6yy42633v2y9anf',
-    'Authorization': 'Bearer wx8vi6yxg9mvgg8t365ekmuka3a1fz'
+QUALITYS = ["original", "hd", "sd"]
+QUALITY_WEIGHTS = {
+    "original": 1080,
+    "hd": 720,
+    "sd": 480
 }
-VALID_URL_BASE = r'(?:https?://)?(?:(?:play|go|m)\.)?afreecatv\.com/(?P<id>[0-9_a-zA-Z]+)'
 
-class Afreeca(SDownload):
+STREAM_INFO_URLS = "{rmd}/broad_stream_assign.html"
+
+VALID_URL_BASE = r"https?://play\.afreecatv\.com/(?P<username>\w+)(?:/\d+)?"
+
+CHANNEL_API_URL = "http://live.afreecatv.com:8057/afreeca/player_live_api.php"
+
+class Afreeca(FFmpegdl):
     def __init__(self, fname, url, suffix='mp4'):
         super().__init__(fname, url, suffix)
+        _url_re = re.compile(VALID_URL_BASE)
+        self.username = _url_re.match(url).group("username")
+
+    def check_stream(self):
+        logger.debug(self.fname)
+        resBNO = requests.post(CHANNEL_API_URL,
+            data={"bid": self.username, "mode": "landing", "player_type": "html5"}, timeout=5)
+        resBNO.close()
+        # print("IS ONLINE = ",resBNO.json()["CHANNEL"]["RESULT"])
+
+        if resBNO.json()["CHANNEL"]["RESULT"] == 0:
+            return False
+        BNO = resBNO.json()["CHANNEL"]["BNO"]
+        CDN = resBNO.json()["CHANNEL"]["CDN"]
+        RMD = resBNO.json()["CHANNEL"]["RMD"]
+        data2 = {
+            "bid": self.username,
+            "bno": BNO,
+            "pwd": "",
+            "quality": QUALITYS[0],
+            "type": "pwd"
+        }
+        resAID = requests.post(CHANNEL_API_URL,data=data2,timeout=5)
+        resAID.close()
+        AID = resAID.json()["CHANNEL"]["AID"]
+        # print("BNO=",BNO,"CDN=",CDN,"RMD=",RMD,"AID=",AID)
+        params = {
+            "return_type": CDN,
+            "broad_key": "{broadcast}-flash-{quality}-hls".format(broadcast=BNO,quality=QUALITYS[0])
+        }
+        res = requests.get(STREAM_INFO_URLS.format(rmd = RMD),params=params,timeout=5)
+        res.close()
+        # print("URL=",res.json()["view_url"]+"?aid="+AID)
+        self.ydl_opts['absurl'] =res.json()["view_url"]+"?aid="+AID
+        return True
 __plugin__ = Afreeca
