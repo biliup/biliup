75b1de1e63f5172fb4290001833cf52909fe5ba2
Memory丶C哥 <554233472@qq.com>
2021-10-12 16:21:39 +0800

    尝试修复程序卡死的问题

--- biliup/plugins/bilibili.py
+++ biliup/plugins/bilibili.py
@@ -27,28 +27,28 @@ class Bilibili(DownloadBase):
         api2_data = api2_data['data']
         if api2_data['live_status'] != 1:
             return False
-        title = api2_data['title']
-        api3_data = \
-            requests.get(f"https://api.live.bilibili.com/live_user/v1/UserInfo/get_anchor_in_room?roomid={vid}").json()
-        if api3_data['code'] == 0:
-            artist = api3_data['data']['info']['uname']
-            title = '{} - {}'.format(title, artist)
-        logger.debug(title)
+        # title = api2_data['title']
+        # api3_data = \
+        #     requests.get(f"https://api.live.bilibili.com/live_user/v1/UserInfo/get_anchor_in_room?roomid={vid}").json()
+        # if api3_data['code'] == 0:
+        #     artist = api3_data['data']['info']['uname']
+        #     title = '{} - {}'.format(title, artist)
+        # logger.debug(title)
         params = {
-            'https_url_req': 1,
+            #'https_url_req': 1,
             'cid': vid,
-            'platform': 'h5',
+            'platform': 'web',
             'qn': 10000,
-            'ptype': '16'
+            #'ptype': '16'
         }
         data = requests.get("https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl", params=params).json()
         if data['code'] != 0:
             logger.debug(data['msg'])
             return False
         data = data['data']
-        qlt = data['current_qn']
-        aqlts = {x['qn']: x['desc'] for x in data['quality_description']}
-        logger.debug(qlt)
-        logger.debug(aqlts)
+        # qlt = data['current_qn']
+        # aqlts = {x['qn']: x['desc'] for x in data['quality_description']}
+        # logger.debug(qlt)
+        # logger.debug(aqlts)
         self.raw_stream_url = random.choice(data['durl'])['url']
         return True
