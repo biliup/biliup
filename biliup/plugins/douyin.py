ea169a23e58f9ba2538e2f43fe1c7b89de9c6ac0
Memory丶C哥 <554233472@qq.com>
2022-03-22 21:11:23 +0800

    修复抖音直播不能录制的问题 (#99)
    * Update douyu.py
    
    现在，支持斗鱼直播间靓号
    
    * Update douyu.py
    
    修复报错问题
    
    * Update download.py
    
    录像文件增加直播间标题名
    
    * Update douyu.py
    
    * Update huya.py
    
    * Update bilibili.py
    
    * Update acfun.py
    
    * Update egame.py
    
    * 尝试修复因网络问题导致重启上传问题
    
    * fix douyu code bug
    
    * Update douyin.py
    
    * Update config(demo).yaml

--- biliup/plugins/douyin.py
+++ biliup/plugins/douyin.py
@@ -4,6 +4,7 @@ import urllib.request
 import requests
 
 from . import logger
+from .. import config
 from ..engine.decorators import Plugin
 from ..engine.download import DownloadBase
 
@@ -21,31 +22,23 @@ class Douyin(DownloadBase):
         headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/94.0.4606.71 Safari/537.36 Edg/94.0.992.38",
-            "referer": "https://live.douyin.com/"
+            "referer": "https://live.douyin.com/",
+            "cookie": f"{config.get('douyin_cookie')}"
         }
-        try:
-            r1 = requests.get('https://live.douyin.com/' + rid, headers=headers).text \
-                .split('<script id="RENDER_DATA" type="application/json">')[1].split('</script>')[0]
-        except IndexError:
-            logger.debug("连接异常")
-            return False
+        r1 = requests.get('https://live.douyin.com/' + rid, headers=headers).text \
+            .split('<script id="RENDER_DATA" type="application/json">')[1].split('</script>')[0]
         r2 = urllib.request.unquote(r1)
-        try:
-            r3 = json.loads(r2)['routeInitialProps']['error']
-            if r3:
-                logger.debug("直播间不存在")
-                return False
-        except KeyError:
-            logger.debug("直播间存在")
-        try:
-            r5 = json.loads(r2)['initialState']['roomStore']['roomInfo']['room']['stream_url']['flv_pull_url']
-            i = 0
-            for k in r5:
-                if i < 1:
-                    r6 = k
-                    i = i + 1
-            self.raw_stream_url = r5[r6]
-            return True
-        except KeyError:
-            logger.debug("主播未开播")
+
+        r3 = json.loads(r2)['routeInitialProps']
+        if r3.get('error'):
+            logger.debug("直播间不存在")
             return False
+        else:
+            r4 = json.loads(r2)['initialState']['roomStore']['roomInfo']
+            if r4.get('room'):
+                room_info = r4['room']
+                if room_info.get('stream_url'):
+                    r5 = room_info['stream_url']['flv_pull_url']
+                    self.raw_stream_url = list(r5.values())[0]
+                    self.room_title = room_info['title']
+                    return True
