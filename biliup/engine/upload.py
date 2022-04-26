7bec41a83c86ba33a0df5a691734aa91ecb605e7
cielpy <beijiu572@gmail.com>
2022-04-27 00:44:14 +0800

    add \n to stdin

--- biliup/engine/upload.py
+++ biliup/engine/upload.py
@@ -79,6 +79,6 @@ class UploadBase:
                     logger.info(f"move to {(dest / path.name).absolute()}")
             if post_processor.get('run'):
                 process = subprocess.run(
-                    post_processor['run'], shell=True, input='\n'.join(map(lambda x: str(Path(x).absolute()), data)),
+                    post_processor['run'], shell=True, input='\n'.join(map(lambda x: str(Path(x).absolute()), data)) + '\n',
                     stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, check=True)
                 logger.info(process.stdout.rstrip())
