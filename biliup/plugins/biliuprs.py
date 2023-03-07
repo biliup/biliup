edd96f916341dfed4c2f4e66f2d86415b0493f8e
haha114514 <47912037+haha114514@users.noreply.github.com>
2023-03-07 15:52:19 +1100

    Update biliuprs.py

--- biliup/plugins/biliuprs.py
+++ biliup/plugins/biliuprs.py
@@ -21,12 +21,10 @@ class BiliWeb(UploadBase):
         self.threads = threads
         self.tid = tid
         self.tags = tags
-        if "cover_path" in self.data:
-            self.cover_path = self.data["cover_path"]
+        if cover_path:
+            self.cover_path = cover_path
         elif "live_cover_path" in self.data:
             self.cover_path = self.data["live_cover_path"]
-        else:
-            self.cover_path = ''
         self.desc = description
         self.dynamic = dynamic
         self.copyright = copyright
@@ -49,7 +47,7 @@ class BiliWeb(UploadBase):
             line = stream_gears.UploadLine.CosInternal
         tag = ','.join(self.tags)
         source = self.data["url"] if self.copyright == 2 else ""
-        #cover = self.cover_path
+        cover = self.cover_path if self.cover_path is not None else ""
         filtered_list = [file for file in file_list if not file.endswith(('.xml', '.webp', '.jpg'))] #自动过滤非视频文件
         dtime = None
         if self.dtime:
@@ -64,7 +62,7 @@ class BiliWeb(UploadBase):
             source,
             self.desc,
             self.dynamic,
-            self.cover_path,
+            cover,
             dtime,
             line,
             self.threads,
