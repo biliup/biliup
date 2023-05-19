56b129f3a40f6853781ebf8ed1396f912b017512
XuanRan_Nyan <31968665+xxxxuanran@users.noreply.github.com>
2023-05-19 16:51:34 +0800

    Some improvements (#439)

--- biliup/plugins/douyu.py
+++ biliup/plugins/douyu.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 import requests
 from ykdl.util.match import match1
 
@@ -17,9 +15,8 @@ class Douyu(DownloadBase):
         self.douyu_danmaku = config.get('douyu_danmaku', False)
 
     def check_stream(self):
-        from ykdl.extractors.douyu.util import ub98484234
         if len(self.url.split("douyu.com/")) < 2:
-            logger.error("直播间地址:" + self.url + " 错误")
+            logger.debug("直播间地址:" + self.url + " 错误")
             return False
         html = requests.get(self.url).text
         vid = match1(html, r'\$ROOM\.room_id\s*=\s*(\d+)',
@@ -27,23 +24,26 @@ class Douyu(DownloadBase):
                      r'"room_id.?":(\d+)',
                      r'data-onlineid=(\d+)')
         if not vid:
-            logger.error("直播间" + vid + "：被关闭或不存在")
+            logger.debug("直播间" + vid + "：被关闭或不存在")
             return False
         roominfo = requests.get(f"https://www.douyu.com/betard/{vid}").json()['room']
         if roominfo['show_status'] != 1 or roominfo['videoLoop'] != 0:
-            logger.error("直播间" + vid + "：未开播或正在放录播")
+            logger.debug("直播间" + vid + "：未开播或正在放录播")
             return False
         self.room_title = roominfo['room_name']
         html_h5enc = requests.get(f'https://www.douyu.com/swf_api/homeH5Enc?rids={vid}').json()
         js_enc = html_h5enc['data']['room' + vid]
         params = {
-            'cdn': config.get('douyucdn', ''),
+            'cdn': config.get('douyucdn', 'tct-h5'),
             'iar': 0,
             'ive': 0,
             'rate': 0,
         }
-        Extractor = namedtuple('Extractor', ['vid', 'logger'])
-        ub98484234(js_enc, Extractor(vid, logger), params)
+        try:
+            ub98484234(js_enc, vid, params)
+        except:
+            logger.error('请至少安装一个 Javascript 解释器')
+            return False
         live_data = get_play_info(vid, self.fake_headers, params)
         if type(live_data) is not dict:
             return False
@@ -61,18 +61,23 @@ class Douyu(DownloadBase):
             self.danmaku.stop()
             logger.info("结束弹幕录制")
 
-def get_play_info(vid, fake_headers, params):
-    import random
-    try:
-        html_content = requests.post(f'https://www.douyu.com/lapi/live/getH5Play/{vid}', headers=fake_headers,
-                                params=params).json()
-        live_data = html_content["data"]
-        # 尝试规避斗鱼自建scdn
-        # scdn 仅在该省市的ISP首次访问上方API后才会新增，且在新增后两分钟内无流可用（404）
-        if not live_data['rtmp_cdn'].endswith('h5'):
-            while not params['cdn'].endswith('h5'):
-                params['cdn'] = random.choice(live_data['cdnsWithName']).get('cdn')
-            return get_play_info(vid, fake_headers, params)
-    except Exception:
-        return None
+def get_play_info(vid, headers, params):
+    html_content = requests.post(f'https://www.douyu.com/lapi/live/getH5Play/{vid}', headers=headers, params=params).json()
+    live_data = html_content["data"]
+    # 禁用斗鱼主线路
+    if not live_data['rtmp_cdn'].endswith('h5') or 'akm' in live_data['rtmp_cdn']:
+        params['cdn'] = 'tct-h5'
+        return get_play_info(vid, headers, params)
     return live_data
+
+def ub98484234(js_enc, vid, params):
+    import jsengine
+    import uuid
+    import time
+    from urllib.parse import parse_qs
+    did = uuid.uuid4().hex
+    tt = str(int(time.time()))
+    crypto_js = requests.get('https://cdn.staticfile.org/crypto-js/4.1.1/crypto-js.min.js').text
+    js = jsengine.JSEngine(js_enc + crypto_js)
+    query = js.call('ub98484234', vid, did, tt)
+    params.update({k: v[0] for k, v in parse_qs(query).items()})
