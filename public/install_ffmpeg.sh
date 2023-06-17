838c09ee442a1007fd370a95a0925aa405fee751
XuanRan_Nyan <31968665+xxxxuanran@users.noreply.github.com>
2023-06-18 07:43:51 +0800

    Update

--- public/install_ffmpeg.sh
+++ public/install_ffmpeg.sh
@@ -4,6 +4,8 @@ install_with_apt() {
   apt-get install --no-install-recommends ffmpeg -y
 }
 
+use_apt=false
+
 case "$(uname -m)" in
   x86_64|amd64)
     arch="64"
@@ -12,14 +14,21 @@ case "$(uname -m)" in
     arch="arm64"
     ;;
   *)
-    install_with_apt
+    use_apt=true
     ;;
 esac
 
 install_without_apt() {
   ffmpeg_filename="ffmpeg-master-latest-linux${arch}-gpl"
-  wget -c "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/$ffmpeg_filename.tar.xz" -O - | tar -xJf - -C /usr/local/bin $ffmpeg_filename/bin/ffmpeg
-  rm -f $ffmpeg_filename.tar.xz
+  wget -c "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/$ffmpeg_filename.tar.xz"
+  echo $ffmpeg_filename.tar.xz
+  tar -xvf $ffmpeg_filename.tar.xz $ffmpeg_filename/bin/ffmpeg
+  mv $ffmpeg_filename/bin/ffmpeg /usr/local/bin/
+  rm -rf $ffmpeg_filename*
 }
 
-install_without_apt
+if $use_apt; then
+  install_with_apt
+else
+  install_without_apt
+fi
