9da22c451f5a1399d204e504aee694e343f46aa2
⁢4.435km/s <54017765+CoolZxp@users.noreply.github.com>
2023-10-18 21:50:29 +0800

    AfreecaTV添加账号密码登陆 (#661)
    AfreecaTV添加直播间标题

--- biliup/plugins/afreecaTV.py
+++ biliup/plugins/afreecaTV.py
@@ -1,51 +1,117 @@
+import time
+from typing import Optional
+
 import requests
 
+from biliup.config import config
+from ..common.tools import NamedLock
 from ..engine.decorators import Plugin
-from ..plugins import match1, logger
 from ..engine.download import DownloadBase
+from ..plugins import match1, logger
 
 # VALID_URL_BASE = r"https?://(.*?)\.afreecatv\.com/(?P<username>\w+)(?:/\d+)?"
 VALID_URL_BASE = r"https?://play\.afreecatv\.com/(?P<username>\w+)(?:/\d+)?"
-STREAM_INFO_URLS = "{rmd}/broad_stream_assign.html"
-CHANNEL_API_URL = "http://live.afreecatv.com/afreeca/player_live_api.php"
+CHANNEL_API_URL = "https://live.afreecatv.com/afreeca/player_live_api.php"
 
-QUALITIES = ["original", "hd", "sd"]
+QUALITIES = ["original", "hd4k", "hd", "sd"]
 
 
 @Plugin.download(regexp=r"https?://(.*?)\.afreecatv\.com/(?P<username>\w+)(?:/\d+)?")
 class AfreecaTV(DownloadBase):
     def __init__(self, fname, url, suffix='flv'):
         super().__init__(fname, url, suffix)
+        if AfreecaTVUtils.get_cookie():
+            self.fake_headers['Cookie'] = ';'.join(
+                [f"{name}={value}" for name, value in AfreecaTVUtils.get_cookie().items()])
 
     def check_stream(self, is_check=False):
-        logger.debug(self.fname)
-        username = match1(self.url, VALID_URL_BASE)
-        res_bno = requests.post(CHANNEL_API_URL + "?bjid=" + username,
-                                data={"bid": username, "mode": "landing", "player_type": "html5"}, timeout=5)
-        res_bno.close()
-        if res_bno.json()["CHANNEL"]["RESULT"] == 0:
-            return
         try:
-            bno = res_bno.json()["CHANNEL"]["BNO"]
-            cdn = res_bno.json()["CHANNEL"]["CDN"]
-            rmd = res_bno.json()["CHANNEL"]["RMD"]
-            res_aid = requests.post(CHANNEL_API_URL, data={
+            username = match1(self.url, VALID_URL_BASE)
+            if not username:
+                logger.warning(f"{AfreecaTV.__name__}: {self.url}: 直播间地址错误")
+                return False
+
+            channel_info = requests.post(CHANNEL_API_URL, data={
                 "bid": username,
-                "bno": bno,
+                "bno": "",
+                "type": "live",
                 "pwd": "",
+                "player_type": "html5",
+                "stream_type": "common",
                 "quality": QUALITIES[0],
-                "type": "pwd"
-            }, timeout=5)
-            res_aid.close()
+                "mode": "landing",
+                "from_api": 0,
+            }, headers=self.fake_headers, timeout=5).json()
+
+            if channel_info["CHANNEL"]["RESULT"] == -6:
+                logger.warning(f"{AfreecaTV.__name__}: {self.url}: 检测失败,请检查账号密码设置")
+                return False
+
+            if channel_info["CHANNEL"]["RESULT"] != 1:
+                return False
+
+            self.room_title = channel_info["CHANNEL"]["TITLE"]
+
+            if is_check:
+                return True
+
+            aid_info = requests.post(CHANNEL_API_URL, data={
+                "bid": username,
+                "bno": channel_info["CHANNEL"]["BNO"],
+                "type": "aid",
+                "pwd": "",
+                "player_type": "html5",
+                "stream_type": "common",
+                "quality": QUALITIES[0],
+                "mode": "landing",
+                "from_api": 0,
+            }, headers=self.fake_headers, timeout=5).json()
+
+            view_info = requests.get(f'{channel_info["CHANNEL"]["RMD"]}/broad_stream_assign.html', params={
+                "return_type": channel_info["CHANNEL"]["CDN"],
+                "broad_key": f'{channel_info["CHANNEL"]["BNO"]}-common-{QUALITIES[0]}-hls'
+            }, headers=self.fake_headers, timeout=5).json()
+
+            self.raw_stream_url = view_info["view_url"] + "?aid=" + aid_info["CHANNEL"]["AID"]
         except:
-            logger.warning(f"afreecaTV {self.url}：获取错误，本次跳过")
+            logger.warning(f"{AfreecaTV.__name__}: {self.url}: 获取错误，本次跳过")
             return False
-        aid = res_aid.json()["CHANNEL"]["AID"]
-        params = {
-            "return_type": cdn,
-            "broad_key": "{broadcast}-flash-{quality}-hls".format(broadcast=bno, quality=QUALITIES[0])
-        }
-        res = requests.get(STREAM_INFO_URLS.format(rmd=rmd), params=params, timeout=5)
-        res.close()
-        self.raw_stream_url = res.json()["view_url"] + "?aid=" + aid
+
         return True
+
+
+class AfreecaTVUtils:
+    _cookie: Optional[dict[str, str]] = None
+    _cookie_expires = None
+
+    @staticmethod
+    def get_cookie() -> Optional[dict[str, str]]:
+        with NamedLock("AfreecaTV_cookie_get"):
+            if not AfreecaTVUtils._cookie or AfreecaTVUtils._cookie_expires <= time.time():
+                username = config.get('user', {}).get('afreecatv_username', '')
+                password = config.get('user', {}).get('afreecatv_password', '')
+                if not username or not password:
+                    return None
+                response = requests.post("https://login.afreecatv.com/app/LoginAction.php", data={
+                    "szUid": username,
+                    "szPassword": password,
+                    "szWork": "login",
+                    "szType": "json",
+                    "isSaveId": "true",
+                    "isSavePw": "true",
+                    "isSaveJoin": "true",
+                    "isLoginRetain": "Y",
+                })
+                if response.json()["RESULT"] != 1:
+                    return None
+
+                cookie_dict = response.cookies.get_dict()
+                AfreecaTVUtils._cookie = {
+                    "RDB": cookie_dict["RDB"],
+                    "PdboxBbs": cookie_dict["PdboxBbs"],
+                    "PdboxTicket": cookie_dict["PdboxTicket"],
+                    "PdboxSaveTicket": cookie_dict["PdboxSaveTicket"],
+                }
+                AfreecaTVUtils._cookie_expires = time.time() + (7 * 24 * 60 * 60)
+
+            return AfreecaTVUtils._cookie
