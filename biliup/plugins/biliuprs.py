adf5e8473cfec7c948e8cc49ae81be6b79aa3ad8
CoolZxp <54017765+CoolZxp@users.noreply.github.com>
2023-11-28 20:06:02 +0800

    避免使用biliup-rs上传后仍旧过多内存

--- biliup/plugins/biliuprs.py
+++ biliup/plugins/biliuprs.py
@@ -1,7 +1,8 @@
+import multiprocessing as mp
 import time
+from typing import List
 
 import stream_gears
-from typing import List
 
 from ..engine import Plugin
 from ..engine.upload import UploadBase, logger
@@ -41,54 +42,44 @@ class BiliWeb(UploadBase):
         self.user_cookie = user_cookie
 
     def upload(self, file_list: List[UploadBase.FileInfo]) -> List[UploadBase.FileInfo]:
-        line = None
-        if self.lines == 'kodo':
-            line = stream_gears.UploadLine.Kodo
-        elif self.lines == 'bda2':
-            line = stream_gears.UploadLine.Bda2
-        elif self.lines == 'ws':
-            line = stream_gears.UploadLine.Ws
-        elif self.lines == 'qn':
-            line = stream_gears.UploadLine.Qn
-        elif self.lines == 'cos':
-            line = stream_gears.UploadLine.Cos
-        elif self.lines == 'cos-internal':
-            line = stream_gears.UploadLine.CosInternal
-        tag = ','.join(self.tags)
         if self.credits:
-            self.desc_v2 = self.creditsToDesc_v2()
+            desc_v2 = self.creditsToDesc_v2()
         else:
-            self.desc_v2 = [{
-                    "raw_text": self.desc,
-                    "biz_id": "",
-                    "type": 1
-                }]
-        source = self.data["url"] if self.copyright == 2 else ""
-        cover = self.cover_path if self.cover_path is not None else ""
-        video_list = [file.video for file in file_list]
-        dtime = None
-        if self.dtime:
-            dtime = int(time.time() + self.dtime)
-        stream_gears.upload(
-            video_list,
-            self.user_cookie,
-            self.data["format_title"][:80],
-            self.tid,
-            tag,
-            self.copyright,
-            source,
-            self.desc,
-            self.dynamic,
-            cover,
-            self.dolby,
-            self.hires,
-            self.no_reprint,
-            self.open_elec,
-            self.threads,
-            self.desc_v2,
-            dtime,
-            line
-        )
+            desc_v2 = [{
+                "raw_text": self.desc,
+                "biz_id": "",
+                "type": 1
+            }]
+
+        ex_parent_conn, ex_child_conn = mp.Pipe()
+        upload_args = {
+            "ex_conn": ex_child_conn,
+            "lines": self.lines,
+            "video_path": [file.video for file in file_list],
+            "cookie_file": self.user_cookie,
+            "title": self.data["format_title"][:80],
+            "tid": self.tid,
+            "tag": ','.join(self.tags),
+            "copyright": self.copyright,
+            "source": self.data["url"] if self.copyright == 2 else "",
+            "desc": self.desc,
+            "dynamic": self.dynamic,
+            "cover": self.cover_path if self.cover_path is not None else "",
+            "dolby": self.dolby,
+            "lossless_music": self.hires,
+            "no_reprint": self.no_reprint,
+            "open_elec": self.open_elec,
+            "limit": self.threads,
+            "desc_v2": desc_v2,
+            "dtime": int(time.time() + self.dtime) if self.dtime else None,
+        }
+
+        upload_process = mp.get_context('spawn').Process(target=stream_gears_upload, daemon=True, kwargs=upload_args)
+        upload_process.start()
+        upload_process.join()
+        if ex_parent_conn.poll():
+            raise RuntimeError(ex_parent_conn.recv())
+
         logger.info(f"上传成功: {self.principal}")
         return file_list
 
@@ -96,10 +87,10 @@ class BiliWeb(UploadBase):
         desc_v2 = []
         desc_v2_tmp = self.desc
         for credit in self.credits:
-            try :
+            try:
                 num = desc_v2_tmp.index("@credit")
                 desc_v2.append({
-                    "raw_text": " "+desc_v2_tmp[:num],
+                    "raw_text": " " + desc_v2_tmp[:num],
                     "biz_id": "",
                     "type": 1
                 })
@@ -109,14 +100,36 @@ class BiliWeb(UploadBase):
                     "type": 2
                 })
                 self.desc = self.desc.replace(
-                    "@credit", "@"+credit["username"]+"  ", 1)
-                desc_v2_tmp = desc_v2_tmp[num+7:]
+                    "@credit", "@" + credit["username"] + "  ", 1)
+                desc_v2_tmp = desc_v2_tmp[num + 7:]
             except IndexError:
                 logger.error('简介中的@credit占位符少于credits的数量,替换失败')
         desc_v2.append({
-            "raw_text": " "+desc_v2_tmp,
+            "raw_text": " " + desc_v2_tmp,
             "biz_id": "",
             "type": 1
         })
         desc_v2[0]["raw_text"] = desc_v2[0]["raw_text"][1:]  # 开头空格会导致识别简介过长
         return desc_v2
+
+
+def stream_gears_upload(ex_conn, lines, *args, **kwargs):
+    try:
+        if lines == 'kodo':
+            kwargs['line'] = stream_gears.UploadLine.Kodo
+        elif lines == 'bda2':
+            kwargs['line'] = stream_gears.UploadLine.Bda2
+        elif lines == 'ws':
+            kwargs['line'] = stream_gears.UploadLine.Ws
+        elif lines == 'qn':
+            kwargs['line'] = stream_gears.UploadLine.Qn
+        elif lines == 'cos':
+            kwargs['line'] = stream_gears.UploadLine.Cos
+        elif lines == 'cos-internal':
+            kwargs['line'] = stream_gears.UploadLine.CosInternal
+        elif lines == 'bldsa':
+            kwargs['line'] = stream_gears.UploadLine.Bldsa
+
+        stream_gears.upload(*args, **kwargs)
+    except Exception as e:
+        ex_conn.send(e)
