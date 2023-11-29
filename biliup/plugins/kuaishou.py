cda930b1e22412503ae742bd45ff8947fb2c7ccb
CoolZxp <54017765+CoolZxp@users.noreply.github.com>
2023-11-29 23:39:02 +0800

    解决快手风控

--- biliup/plugins/kuaishou.py
+++ biliup/plugins/kuaishou.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 from ..engine.decorators import Plugin
@@ -20,25 +22,31 @@ class Kuaishou(DownloadBase):
 
             with requests.Session() as s:
                 s.headers = self.fake_headers.copy()
-                # 首页低风控生成did
+                s.headers["Upgrade-Insecure-Requests"] = "1"
+                # 首页低风控生成did client_key
                 s.get("https://live.kuaishou.com", timeout=5)
-
+                s.get("https://live.kuaishou.com/live_api/home/list", timeout=5)
+                s.get("https://live.kuaishou.com/live_api/home/category", timeout=5)
+                time.sleep(1)
                 room_info = s.get(
-                    f"https://live.kuaishou.com/live_api/liveroom/livedetail?principalId={room_id}",
+                    f"https://live.kuaishou.com/live_api/profile/public?count=0&principalId={get_kwaiId(self.url)}",
                     timeout=5).json()['data']
 
-            if room_info['result'] == 22:
+            if room_info['result'] == 22 or room_info['result'] == 677:
                 logger.warning(f"{Kuaishou.__name__}: {self.url}: 直播间地址错误")
                 return False
-            if room_info['result'] == 671:
-                logger.debug(f"{Kuaishou.__name__}: {self.url}: 直播间未开播或非直播")
+            if room_info['result'] == 2:
+                logger.warning(f"{Kuaishou.__name__}: {self.url}: 已风控")
                 return False
             if room_info['result'] != 1:
                 logger.warning(f"{Kuaishou.__name__}: {self.url}: 错误{room_info['result']}")
                 return False
+            if len(room_info['live']['playUrls']) <= 0:
+                logger.debug(f"{Kuaishou.__name__}: {self.url}: 未开播")
+                return False
 
-            self.room_title = room_info['liveStream']['caption']
-            self.raw_stream_url = room_info['liveStream']['playUrls'][0]['adaptationSet']['representation'][-1]['url']
+            self.room_title = room_info['live']['caption']
+            self.raw_stream_url = room_info['live']['playUrls'][0]['adaptationSet']['representation'][-1]['url']
         except:
             logger.warning(f"{Kuaishou.__name__}: {self.url}: 获取错误，本次跳过")
             return False
