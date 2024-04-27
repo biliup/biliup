1eeacf939cb999a524011162538879d597353bf3
Kataick <24969684+Kataick@users.noreply.github.com>
2024-04-27 19:01:48 +0800

    防止分段后处理超出预期的执行次数 (#895)
    * 防止分段后处理超出预期的执行次数
    
    * 改进
    
    * 同步改动后的processor引用

--- biliup/engine/download.py
+++ biliup/engine/download.py
@@ -244,20 +244,20 @@ class DownloadBase(ABC):
                 logger.exception('Uncaught exception:')
             finally:
                 self.close()
-                try:
-                    segment_processor = config['streamers'].get(self.fname, {}).get('segment_processor')
-                    if segment_processor:
-                        from biliup.common.tools import processor
-                        from multiprocessing import Process
-                        from biliup.database.db import get_file_list
-                        with SessionLocal() as db:
-                            file_name = get_file_list(db, self.database_row_id)[-1]
-                        segment_process = Process(target=processor, args=(segment_processor, os.path.abspath(f'{file_name}.{self.suffix}') + ('\n' + os.path.abspath(f'{file_name}.xml') if os.path.exists(os.path.abspath(f'{file_name}.xml')) else '')))
-                        segment_process.start()
-                except:
-                    logger.exception('Uncaught exception:')
             if ret:
                 if self.is_download:
+                    try:
+                        segment_processor = config['streamers'].get(self.fname, {}).get('segment_processor')
+                        if segment_processor:
+                            from biliup.common.tools import processor
+                            import multiprocessing as mp
+                            from biliup.database.db import get_file_list
+                            with SessionLocal() as db:
+                                file_name = get_file_list(db, self.database_row_id)[-1]
+                            segment_process = mp.get_context('spawn').Process(target=processor, daemon=True, kwargs=(segment_processor, os.path.abspath(f'{file_name}.{self.suffix}') + ('\n' + os.path.abspath(f'{file_name}.xml') if os.path.exists(os.path.abspath(f'{file_name}.xml')) else '')))
+                            segment_process.start()
+                    except:
+                        logger.exception('Uncaught exception:')
                     # 成功下载后也不检测下一个需要下载的视频而是先上传等待下次检测保证上传时使用下载视频的标题
                     # 开启边录边传会快些
                     break
