37bbcfad3d84cc608cd74097ee6d9d60b284c310
Kataick <24969684+Kataick@users.noreply.github.com>
2024-08-12 22:34:14 +0800

    flx(build): 修复#1025 npm会编译失败的问题 (#1031)
    * flx(build): #1025
    
    * 优化
    
    * flx(build): 修复构建时只会从主库中获取文件  (#1034)
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: ForgQi <34411314+ForgQi@users.noreply.github.com>

--- app/job/page.tsx
+++ app/job/page.tsx
@@ -3,13 +3,13 @@
 import { AutoComplete, Layout, Nav, Spin, Table, Typography } from "@douyinfe/semi-ui";
 import { SortOrder } from "@douyinfe/semi-ui/lib/es/table";
 import useSWR from "swr";
-import {fetcher, FileList} from "@/app/lib/api-streamer";
+import { fetcher, FileList } from "@/app/lib/api-streamer";
 import {
     JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useRef,
     useState
 } from "react";
-import {IconCustomerSupport, IconSearch, IconVideoListStroked} from "@douyinfe/semi-icons";
-import {humDate} from "@/app/lib/utils";
+import { IconCustomerSupport, IconSearch, IconVideoListStroked } from "@douyinfe/semi-icons";
+import { humDate } from "@/app/lib/utils";
 import Filter from "@/app/job/Filter";
 
 export default function Home() {
@@ -48,7 +48,7 @@ export default function Home() {
         {
             title: '更新日期',
             dataIndex: 'date',
-            defaultSortOrder: 'descend',
+            defaultSortOrder: 'descend' as SortOrder,
             sorter: (a: any, b: any) => ( a.date - b.date > 0 ? 1 : -1),
             render: (time: number) => humDate(time),
         },
