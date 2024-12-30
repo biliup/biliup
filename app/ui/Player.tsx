4f0367275a2d6a6d00b6867961207f3db25a62e4
xxxxuanran <xxxxuanran@gmail.com>
2024-12-31 02:14:44 +0800

    使用 artplayer 替换 西瓜播放器

--- app/ui/Player.tsx
+++ app/ui/Player.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import Player from 'xgplayer';
+import Artplayer from 'artplayer';
 import 'xgplayer/dist/index.min.css';
 import FlvPlugin from "xgplayer-flv";
 import FlvJsPlugin from 'xgplayer-flv.js'
@@ -7,20 +8,33 @@ import Mp4Plugin from "xgplayer-mp4";
 
 const Players: React.FC<{url: string}> = ({url}) => {
     useEffect(() => {
-        let player: Player | null = new Player({
-            id: 'mse',
+        // let player: Player | null = new Player({
+        //     id: 'mse',
+        //     url: url,
+        //     height: '100%',
+        //     plugins: [FlvPlugin, Mp4Plugin],
+        //     // plugins: [FlvJsPlugin],
+        //     width: '100%',
+        // });
+        // return () => {
+        //     player?.destroy();
+        //     player = null;
+        // };
+        let player: Artplayer | null = new Artplayer({
+            container: '.artplayer-app',
             url: url,
-            height: '100%',
-            plugins: [FlvPlugin, Mp4Plugin],
-            // plugins: [FlvJsPlugin],
-            width: '100%',
+            autoSize: true,
+            fullscreen: true,
+            fullscreenWeb: true,
+            autoOrientation: true,
+            plugins: [],
         });
         return () => {
             player?.destroy();
             player = null;
         };
     }, [url])
-    return  <div id="mse"></div>;
+    return  <div className="artplayer-app" style={{width: '100%', height: '100%'}}></div>;
 }
 
 export default Players;
