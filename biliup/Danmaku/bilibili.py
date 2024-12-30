8db4a9406f7ac66ab9d59f844e95e7c3eef3ed0b
unknown-o <yushen@mr-wu.top>
2024-12-30 06:12:42 +0000

    feat: B站弹幕服务支持传入cookie

--- biliup/Danmaku/bilibili.py
+++ biliup/Danmaku/bilibili.py
@@ -22,20 +22,42 @@ class Bilibili:
         'accept-language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
         'user-agent': random_user_agent(),
         'origin': 'https://live.bilibili.com',
-        'referer': 'https://live.bilibili.com',
+        'referer': 'https://live.bilibili.com'
     }
 
     @staticmethod
     async def get_ws_info(url, content):
+        # 获取传入的cookie
+        cookie_str = content.get('cookie', None)
+        if cookie_str == "":
+            cookie_str == None
+        buvid3 = None
+        buid = 0
+        is_login = False
+
         danmu_wss_url = 'wss://broadcastlv.chat.bilibili.com/sub'
         room_id = content.get('room_id')
+        if content.get('cookie', None):
+            Bilibili.headers['cookie'] = content.get('cookie')
         async with aiohttp.ClientSession(headers=Bilibili.headers) as session:
             if not room_id:
                 async with session.get("https://api.live.bilibili.com/room/v1/Room/room_init?id=" + match1(url, r'/(\d+)'),
                                    timeout=5) as resp:
                     room_json = await resp.json()
                     room_id = room_json['data']['room_id']
-            async with session.get(f"https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id={room_id}",
+            # 获取录播号cookie信息
+            if content.get('cookie', None):
+                try:
+                    async with session.get(f"https://www.bilibili.com/", timeout=5) as resp:
+                        buvid3 = session.cookie_jar.filter_cookies(f"https://www.bilibili.com/").get('buvid3').value
+                    async with session.get(f"https://api.bilibili.com/x/web-interface/nav", timeout=5) as resp:
+                        resp_data = await resp.json()
+                        buid = resp_data["data"]["mid"]
+                    is_login = True
+                except:
+                    buid = 0
+                    pass
+            async with session.get(f"https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?type=0&id={room_id}",
                                    timeout=5) as resp:
                 danmu_info = await resp.json()
                 danmu_token = danmu_info['data']['token']
@@ -46,16 +68,18 @@ class Bilibili:
                 except:
                     pass
 
-            data = json.dumps({
-                'uid': 0,
+            w_data = {
+                'uid': buid,
                 'roomid': room_id,
                 'protover': 3,
                 'platform': 'web',
                 'type': 2,
                 'key': danmu_token,
-            }, separators=(',', ':')).encode('ascii')
+            }
+            if is_login:
+                w_data['buvid'] = buvid3
+            data = json.dumps(w_data).encode('utf-8')
             reg_datas = [(pack('>i', len(data) + 16) + b'\x00\x10\x00\x01' + pack('>i', 7) + pack('>i', 1) + data)]
-
         return danmu_wss_url, reg_datas
 
     @staticmethod
@@ -91,7 +115,6 @@ class Bilibili:
             return dm_list
 
         dm_list = decode_packet(data)
-
         for dm in dm_list:
             try:
                 msg = {}
@@ -102,11 +125,24 @@ class Bilibili:
                         'DANMU_MSG': 'danmaku',
                         'WELCOME': 'enter',
                         'NOTICE_MSG': 'broadcast',
+                        'SUPER_CHAT_MESSAGE': 'super_chat',
                         'LIVE_INTERACTIVE_GAME': 'interactive_danmaku'  # 新增互动弹幕，经测试与弹幕内容一致
                     }.get(j.get('cmd'), 'other')
                     # 2021-06-03 bilibili 字段更新, 形如 DANMU_MSG:4:0:2:2:2:0
                     if msg.get('msg_type', 'UNKNOWN').startswith('DANMU_MSG'):
                         msg['msg_type'] = 'danmaku'
+
+                    if msg['msg_type'] == 'super_chat':
+                        msg['name'] = j.get('data', {}).get('uname', '')
+                        msg['uid'] = j.get('data', {}).get('uid', '')
+                        msg['content'] = j.get('data', {}).get('message', '')
+                        msg['price'] = j.get('data', {}).get('price', '')
+
+                    if msg['msg_type'] == 'gift':
+                        msg['name'] = j.get('data', {}).get('uname', '')
+                        msg['uid'] = j.get('data', {}).get('uid', '')
+                        msg['content'] = j.get('data', {}).get('giftName', '')
+                        msg['price'] = j.get('data', {}).get('price', '')
 
                     if msg['msg_type'] == 'danmaku':
                         msg['name'] = (j.get('info', ['', '', ['', '']])[2][1] or
