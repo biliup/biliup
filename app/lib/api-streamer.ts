747f07eb11a46ddd9ac46f6953f623b31a6bfc1e
elio <rruinmi1@gmail.com>
2025-03-27 13:56:25 +0800

    录制时间范围前端改为选择

--- app/lib/api-streamer.ts
+++ app/lib/api-streamer.ts
@@ -101,7 +101,7 @@ export interface LiveStreamerEntity {
 	upload_id?: number;
 	status?: string | React.ReactNode;
 	format?: string;
-    time_range?: string;
+    time_range?: string | Date[];
 	preprocessor?: Record<'run', string>[];
 	segment_processor?: Record<'run', string>[];
 	downloaded_processor?: Record<'run', string>[];
