1734ff9f2934847402e2436704e2eb7b7e6867b9
dreammu <84692291+dreammu@users.noreply.github.com>
2025-03-27 22:25:24 +0800

    fix: 修复twitch插件中streamlink的行为

--- biliup/engine/download.py
+++ biliup/engine/download.py
@@ -205,7 +205,7 @@ class DownloadBase(ABC):
                 '-c',
                 'copy',
             ]
-            if use_streamlink:
+            if use_streamlink and not self.raw_stream_url.startswith('http://localhost:'):
                 streamlink_cmd = [
                     'streamlink',
                     '--stream-segment-threads', '3',
