244e03c38ad590628a349b6803fab1e6240ac051
XiaoMiku01 <XiaoMiku01@outlook.com>
2025-08-11 23:41:06 +0800

    fix: 修复遗漏的接口鉴权 (#1417)

--- app/ui/QRcode.tsx
+++ app/ui/QRcode.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { fetcher, sendRequest } from '@/app/lib/api-streamer'
+import { fetcher, sendRequest, proxy } from '@/app/lib/api-streamer'
 import { QRCodeSVG } from 'qrcode.react'
 import { Notification, Spin, Typography } from '@douyinfe/semi-ui'
 
@@ -17,34 +17,34 @@ const Qrcode: React.FC<QrcodeProps> = ({ onSuccess }) => {
     signal.addEventListener('abort', () => {
       console.log('aborted!')
     })
-    ;(async () => {
-      let qrData = await fetcher('/v1/get_qrcode', undefined)
-      setUrl(qrData['data']['url'])
-      console.log(qrData['data']['url'])
-      let res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER ?? ''}/v1/login_by_qrcode`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(qrData),
-        signal: signal,
+      ; (async () => {
+        let qrData = await fetcher('/v1/get_qrcode', undefined)
+        setUrl(qrData['data']['url'])
+        console.log(qrData['data']['url'])
+        let res = await proxy('/v1/login_by_qrcode', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(qrData),
+          signal: signal,
+        })
+        if (!(res.status >= 200 && res.status < 300)) {
+          throw new Error(await res.text())
+        }
+        const data = await res.json()
+        if (!res.ok) {
+          throw new Error(data.message)
+        }
+        onSuccess(data['filename'])
+      })().catch(e => {
+        console.log(e)
+        Notification.error({
+          title: 'QRcode',
+          content: <Typography.Paragraph style={{ maxWidth: 450 }}>{e.message}</Typography.Paragraph>,
+          style: { width: 'min-content' },
+        })
       })
-      if (!(res.status >= 200 && res.status < 300)) {
-        throw new Error(await res.text())
-      }
-      const data = await res.json()
-      if (!res.ok) {
-        throw new Error(data.message)
-      }
-      onSuccess(data['filename'])
-    })().catch(e => {
-      console.log(e)
-      Notification.error({
-        title: 'QRcode',
-        content: <Typography.Paragraph style={{ maxWidth: 450 }}>{e.message}</Typography.Paragraph>,
-        style: { width: 'min-content' },
-      })
-    })
 
     return () => {
       controller.abort('qrcode exit')
